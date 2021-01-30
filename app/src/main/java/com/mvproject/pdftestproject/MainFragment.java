2784303abcee652a0e92f19206241643aba08dd8
viktor <awsom7@gmail.com>
2021-01-30 20:28:36 +0200

    refactor mainfragment

--- app/src/main/java/com/mvproject/pdftestproject/MainFragment.java
+++ app/src/main/java/com/mvproject/pdftestproject/MainFragment.java
@@ -12,25 +12,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.snackbar.Snackbar;
-import com.mvproject.pdftestproject.databinding.FragmentFirstBinding;
+import com.mvproject.pdftestproject.databinding.FragmentMainBinding;
 import com.mvproject.pdftestproject.text.MotionEntity;
 import com.mvproject.pdftestproject.text.TextEntity;
 import com.mvproject.pdftestproject.text.TextLayer;
-import com.mvproject.pdftestproject.utils.Font;
-import com.mvproject.pdftestproject.utils.FontProvider;
 import com.mvproject.pdftestproject.view.MotionView;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
-public class FirstFragment extends Fragment {
+public class MainFragment extends Fragment {
     private float currentZoomLevel = 12;
-    private FragmentFirstBinding binding;
-    private FontProvider fontProvider;
+    private FragmentMainBinding binding;
+    private MainFragmentViewmodel mainFragmentViewmodel;
     private static final String FILENAME = "test_pdf_document.pdf";
 
     @Override
@@ -38,13 +34,14 @@ public class FirstFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = FragmentMainBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        this.fontProvider = new FontProvider(getResources());
+
+        mainFragmentViewmodel = new ViewModelProvider(requireActivity()).get(MainFragmentViewmodel.class);
 
         final MotionView.MotionViewCallback motionViewCallback = new MotionView.MotionViewCallback() {
             @Override
@@ -67,40 +64,8 @@ public class FirstFragment extends Fragment {
 
         binding.motionView.setMotionViewCallback(motionViewCallback);
 
-        try {
-            File file = new File(requireContext().getCacheDir(), FILENAME);
-            if (!file.exists()) {
-                // Since PdfRenderer cannot handle the compressed asset file directly, we copy it into
-                // the cache directory.
-                InputStream asset = requireContext().getAssets().open(FILENAME);
-                FileOutputStream output = new FileOutputStream(file);
-                final byte[] buffer = new byte[1024];
-                int size;
-                while ((size = asset.read(buffer)) != -1) {
-                    output.write(buffer, 0, size);
-                }
-                asset.close();
-                output.close();
-            }
-            ParcelFileDescriptor parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
-            // This is the PdfRenderer we use to render the PDF.
-            if (parcelFileDescriptor != null) {
-                PdfRenderer renderer = new PdfRenderer(parcelFileDescriptor);
-                PdfRenderer.Page page = renderer.openPage(0);
-                int newWidth = (int) (getResources().getDisplayMetrics().widthPixels * page.getWidth() / 72 * currentZoomLevel / 40);
-                int newHeight = (int) (getResources().getDisplayMetrics().heightPixels * page.getHeight() / 72 * currentZoomLevel / 64);
-                Bitmap bitmap = Bitmap.createBitmap(newWidth,newHeight,Bitmap.Config.ARGB_8888);
-                page.render(bitmap,null,null,PdfRenderer.Page.RENDER_MODE_FOR_PRINT);
-                binding.image.setImageBitmap(bitmap);
-                page.close();
-                renderer.close();
-            }
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        ParcelFileDescriptor pdfDescriptor = mainFragmentViewmodel.loadPdfFromAssets(FILENAME);
+        renderPdf(pdfDescriptor);
     }
 
     @Override
@@ -110,12 +75,12 @@ public class FirstFragment extends Fragment {
     }
 
     protected void addTextSticker() {
-        TextLayer textLayer = createTextLayer();
+        TextLayer textLayer = mainFragmentViewmodel.createTextLayer();
         TextEntity textEntity = new TextEntity(
                 textLayer,
                 binding.motionView.getWidth(),
                 binding.motionView.getHeight(),
-                fontProvider
+                mainFragmentViewmodel.getFontProvider()
         );
 
         binding.motionView.addEntityAndPosition(textEntity);
@@ -131,17 +96,21 @@ public class FirstFragment extends Fragment {
                 .setAction("Action", null).show();
     }
 
-
-    private TextLayer createTextLayer() {
-        TextLayer textLayer = new TextLayer();
-        Font font = new Font();
-
-        font.setColor(TextLayer.Limits.INITIAL_FONT_COLOR);
-        font.setSize(TextLayer.Limits.INITIAL_FONT_SIZE);
-        font.setTypeface(fontProvider.getDefaultFontName());
-        textLayer.setFont(font);
-        textLayer.setText("Sign here");
-
-        return textLayer;
+    private void renderPdf(ParcelFileDescriptor parcelFileDescriptor) {
+        try {
+            if (parcelFileDescriptor != null) {
+                PdfRenderer renderer = new PdfRenderer(parcelFileDescriptor);
+                PdfRenderer.Page page = renderer.openPage(0);
+                int newWidth = (int) (getResources().getDisplayMetrics().widthPixels * page.getWidth() / 72 * currentZoomLevel / 40);
+                int newHeight = (int) (getResources().getDisplayMetrics().heightPixels * page.getHeight() / 72 * currentZoomLevel / 64);
+                Bitmap bitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888);
+                page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_PRINT);
+                binding.image.setImageBitmap(bitmap);
+                page.close();
+                renderer.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
