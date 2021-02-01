85eb2b75ae9cf74fd2a67d801e6f0d411634fce3
viktor <awsom7@gmail.com>
2021-02-01 12:39:34 +0200

    remove bar menu

--- app/src/main/java/com/mvproject/pdftestproject/MainActivity.java
+++ app/src/main/java/com/mvproject/pdftestproject/MainActivity.java
@@ -29,22 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void callAddTextView(){
         FragmentManager fm = getSupportFragmentManager();
         MainFragment fragment = (MainFragment)fm.findFragmentById(R.id.firstFragment);
